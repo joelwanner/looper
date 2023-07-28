21560dea1b5c26302acba61aa27d6c6364896d68
Joel Wanner <joel.wanner@adnovum.ch>
2023-07-28 21:48:55 +0200

    Add config

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
+import tomllib
+
+cfg = {}
 
 
 def convert():
     pass
 
+
 def copy():
     pass
 
-if __name__ == '__main__':
-    pass
 
+if __name__ == '__main__':
+    with open("config.toml", "rb") as f:
+        cfg = tomllib.load(f)
+    print(cfg)
